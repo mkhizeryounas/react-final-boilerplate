b91b913b2b948dda5f4f4e8072b1660ccf7e5ac5
Khizer Younas <m.khizeryounas@gmail.com>
2019-02-08 21:48:42 +0500

    router added

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,40 @@
 import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+
 class Home extends Component {
-  state = {};
+  constructor(props) {
+    super(props);
+  }
+  state = {
+    name: ""
+  };
+  setInputValue = (e, inputName) => {
+    this.setState({ [inputName]: e.target.value });
+  };
+  submitForm() {
+    console.log(this.state);
+    this.props.history.push(`/welcome/${this.state.name}`);
+  }
   render() {
-    return <h1>This home</h1>;
+    return (
+      <div>
+        <h1>This home</h1>
+        <br />
+        <div className="form-group">
+          <label>Name</label>
+          <input
+            type="text"
+            className="form-control"
+            value={this.state.name}
+            onChange={e => this.setInputValue(e, "name")}
+          />
+        </div>
+        <button className="btn btn-primary" onClick={() => this.submitForm()}>
+          Submit
+        </button>
+      </div>
+    );
   }
 }
 
-export default Home;
+export default withRouter(Home);
--- src/pages/welcome.js
+++ src/pages/welcome.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
 
 class Welcome extends Component {
+  constructor(props) {
+    super(props);
+  }
   state = {};
   render() {
     return <h1>Welcome, {this.props.match.params.name}</h1>;
